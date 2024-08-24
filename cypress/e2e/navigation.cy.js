91e586e2d042880f07cf3e3ccab4491db958ca9c
amirmirs <130896049+amirmirs@users.noreply.github.com>
2024-08-24 13:06:15 +0000

    added more navigation tests

--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -27,5 +27,42 @@ describe('Navigation Tests', () => {
       cy.url().should('eq', 'http://localhost:3000/statistics');
       cy.contains('Cluster').should('be.visible');
     });
+  
+    // New tests for navigating back from 'About' and 'Statistics' pages
+    it('should navigate from the About page back to the Home page', () => {
+      // First, navigate to the About page
+      cy.visit('http://localhost:3000/about');
+  
+      // Click the 'Home' button
+      cy.contains('Home').click();
+  
+      // Verify that the URL is correct
+      cy.url().should('eq', 'http://localhost:3000/');
+      cy.contains('Disko').should('be.visible');
+    });
+  
+    it('should navigate from the Statistics page back to the Home page', () => {
+      // First, navigate to the Statistics page
+      cy.visit('http://localhost:3000/statistics');
+  
+      // Click the 'Home' button
+      cy.contains('Home').click();
+  
+      // Verify that the URL is correct
+      cy.url().should('eq', 'http://localhost:3000/');
+      cy.contains('Disko').should('be.visible');
+    });
+  
+    it('should navigate from the Statistics page to the About page', () => {
+      // First, navigate to the Statistics page
+      cy.visit('http://localhost:3000/statistics');
+  
+      // Click the 'About' button
+      cy.contains('About').click();
+  
+      // Verify that the URL is correct
+      cy.url().should('eq', 'http://localhost:3000/about');
+      cy.contains('About').should('be.visible');
+    });
   });
   
